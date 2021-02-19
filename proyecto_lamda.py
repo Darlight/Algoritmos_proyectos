3372881a6c12b082a14696e952ff38ffe7952ae6
Mario Andrés Perdomo López <andres.perdomo09@gmail.com>
2021-02-18 21:21:06 -0600

    Comenzando el proyecto

---
+++ proyecto_lamda.py
@@ -0,0 +1,19 @@
+"""
+Universidad del Valle de Guatemala
+CC3041 - Analisis y diseño de algoritmos
+Ing. Tomas Galvez
+Mario Perdomo
+Carnet 18029
+proyecto_lamda.py 
+"""
+
+f = lambda x: x+1
+g = lambda x: 2 * x
+h = lambda x, y: x**2 + y**2
+
+  
+print("f(5) = " + str(f(5)))
+print("g(3) = " + str(g(3)))
+print("g(4,4) = " + str(h(4,4)))
+#print("g({}) = {}".format(2, g(2)))
+#print("h({}, {}) = {}".format(3, 4, h(3, 4)))
