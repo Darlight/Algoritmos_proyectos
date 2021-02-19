dc73838fe01a7708e9e5dbecaaa4d19067209638
Mario Andrés Perdomo López <andres.perdomo09@gmail.com>
2021-02-19 14:35:13 -0600

    Update proyecto_lamda.py

--- proyecto_lamda.py
+++ proyecto_lamda.py
@@ -6,14 +6,25 @@ Mario Perdomo
 Carnet 18029
 proyecto_lamda.py 
 """
-
+#Usando la manera pythonica de los lambdas: https://realpython.com/python-lambda/ 
 f = lambda x: x+1
 g = lambda x: 2 * x
 h = lambda x, y: x**2 + y**2
+cero = lambda fn, x:  fn + x
+uno = lamnda x: x
+
 
   
 print("f(5) = " + str(f(5)))
 print("g(3) = " + str(g(3)))
 print("g(4,4) = " + str(h(4,4)))
+print("cero() = " + str(cero(0,0)))
+print("uno() =")
+print("dos() =")
+print("tres() =")
+print("sucesor() =")
+print("suma() = ")
+print("multi() = ")
+
 #print("g({}) = {}".format(2, g(2)))
 #print("h({}, {}) = {}".format(3, 4, h(3, 4)))
